8ad1cab388f9c8e845a38f3a198cf2862687d7be
Beneq <bquibc@miumg.edu.gt>
2020-10-06 23:51:49 -0600

    mostrar empleados 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import {Employee}from './models/employee'; 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'crud';
+  employeearray: Employee[]=[
+    {id:1,name:"Benito",country:"Guatemala"},
+    {id:2,name:"Juan",country:"Argentina"},
+    {id:3,name:"Jose",country:"USA"},
+  ];
+
 }
